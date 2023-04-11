60707a3724e6008a76d2aa54a01fb37c6403f913
Captainrock1 <Captainrockp1@gmail.com>
2023-04-11 11:11:20 +0700

    Version 1

---
+++ lib/content.ts
@@ -0,0 +1,190 @@
+export const nav = {
+  logo: 'Peerapon_Chaijittiprasert',
+  links: [
+    {
+      text: 'PROJECT',
+      to: '/projects',
+    },
+    {
+      text: 'WORK',
+      to: '/work',
+    },
+    {
+      text: 'MyTeam',
+      to: '/myteam',
+    },
+    {
+      text: 'CONTACT',
+      to: '/contact',
+    },
+    {
+      text: 'Web ETkd',
+      to:"https://webapp-etkd.cyclic.app/",
+    }
+  ],
+};
+
+export const landing = {
+  img: {
+    url: 'https://media.licdn.com/dms/image/D5603AQHkEb6fkHt28A/profile-displayphoto-shrink_800_800/0/1679029164676?e=1686787200&v=beta&t=6MUfE4fCx15Dsn_ElFvjQeextt_w-Y9LBPTxwbwrjEo',
+    alt: 'Peerapon Chaijittiprasert',
+  },
+  text: ['Hi !', "I'm", 'Peerapon Chaijittiprasert.', "I'm a"],
+  btnText: 'Get in Touch',
+};
+
+export const about = [
+  "This website was created by Mr. Peerapon Chaijittiprasert (Captain)🌟🌟",
+  '',
+  "🔥 I am a student from Naresuan University who codes for fun. I have been in coding since 2019 and I'm a software developer from Phitsanulok Thailand. 🔥",
+  '',
+  '🌈 I enjoy developing simple and beautiful application to solve complex problems. When I am not coding, watching movies or reading books for relaxing.',
+  ''
+];
+
+export const techstack = [
+  {
+    img: '/assets/stack/kotlin.svg',
+    name: 'Android',
+  },
+  {
+    img: '/assets/stack/fastapi.svg',
+    name: 'FastAPI',
+  },
+  {
+    img: '/assets/stack/python.svg',
+    name: 'Python',
+  },
+  {
+    img: '/assets/stack/react.svg',
+    name: 'React',
+  },
+  {
+    img: '/assets/stack/vue.svg',
+    name: 'Vue',
+  },
+  {
+     img: '/assets/stack/nextjs.svg',
+     name: 'Next JS',
+  },
+  {
+    img: '/assets/stack/nodejs.svg',
+    name: 'Node',
+  },
+];
+
+export const contactInfo = {
+  heading: ['Get in ', 'Touch'],
+  primary: [
+    {
+      img: '/assets/contact/Telegram.svg',
+      link: 'https://t.me/Captainrockp1',
+      name: 'Telegram',
+    },
+    {
+      img: '/assets/contact/email.svg',
+      link: 'mailto:Captainrockp1@gmail.com',
+      name: 'Mail',
+    },
+  ],
+  social: [
+    {
+      img: '/assets/contact/Github.svg',
+      link: 'https://github.com/Captainrock1',
+      name: 'GitHub',
+    },
+    {
+      img: '/assets/contact/Instagram.svg',
+      link: 'https://www.instagram.com/captainrockp1',
+      name: 'Instagram',
+    },
+    {
+      img: '/assets/contact/Twitter.svg',
+      link: 'https://twitter.com/Captainrockp1',
+      name: 'Twitter',
+    },
+    {
+      img: '/assets/contact/LinkedIN.svg',
+      link: 'https://www.linkedin.com/in/peerapon-chaijittiprasert-991348252',
+      name: 'LinkedIN',
+    },
+  ],
+};
+
+/*export const resume = {
+  url: '',
+  btn: 'DOWNLOAD RESUME',
+};*/
+
+export const projectsInfo = [
+  {
+    title: 'web_etkd',
+    subtitle: 'Web Application for Exchanging and Trading Knowledge Documents.',
+    img: 'assets/projects/WebETKD.jpg',
+    redirect: '',
+    github: 'https://github.com/Captainrock1/Final-Project',
+    link: 'https://webapp-etkd.cyclic.app/',
+  },
+  {
+    title: 'resource_capacity_plan',
+    subtitle: 'Web Application - Resource Capacity Plan for IBM Manage Service Project',
+    img: 'assets/projects/resource_capacity_plan.jpg',
+    redirect: '',
+    link: 'https://drive.google.com/file/d/1ENUHKITTS4T8Invp23jrot3OLp75HIEf/view?usp=sharing',
+  },
+  {
+    title: 'crypto_currency',
+    subtitle: 'a basic tic tac toe game created in flutter',
+    img: 'assets/projects/cryptotax.jpg',
+    redirect: '',
+    github: 'https://github.com/Captainrock1/CryptoTaxTest',
+    link: 'https://www.itax.in.th/pedia/%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%84%E0%B8%B3%E0%B8%99%E0%B8%A7%E0%B8%93%E0%B8%A0%E0%B8%B2%E0%B8%A9%E0%B8%B5%E0%B8%84%E0%B8%A3%E0%B8%B4%E0%B8%9B%E0%B9%82%E0%B8%97-%E0%B8%A0%E0%B8%B2%E0%B8%A9%E0%B8%B5%E0%B9%80%E0%B8%87%E0%B8%B4%E0%B8%99%E0%B9%84%E0%B8%94%E0%B9%89%E0%B8%9A%E0%B8%B8%E0%B8%84%E0%B8%84%E0%B8%A5%E0%B8%98%E0%B8%A3%E0%B8%A3%E0%B8%A1%E0%B8%94%E0%B8%B2/?fbclid=IwAR1s81IDDnSyrFKoP0lzLFKa7ScrPvLD1yTwIJtNgZeUX66-3Ku1Q3eneqI',
+  },  
+];
+
+
+export const myteamInfo = [
+  {
+    name: 'Kanaphong Aiamtip',
+    nickname:'Nick Name Soda',
+    penname: 'Pen Name Sea_taley',
+    img: 'assets/soda.jpg',
+    website: '',
+    github: 'https://github.com/KanapongAiamtip',
+    fb: 'https://www.facebook.com/profile.php?id=100006123256688',
+    ig:'https://www.instagram.com/_______knp___krit/',
+  },
+];
+
+export const workInfo = [
+  {
+    title: 'IBM Thailand',
+    url: 'https://www.ibm.com/planetwide/th/',
+    img: 'assets/work/ibm.jpg',
+    imgDark: 'assets/work/ibm.jpg',
+    duration: '13 Jun 2022 -30 Sep 2022',
+    role: 'Application developer (Intern)',
+    about: [
+      'Developed and deployed a Node.js-based web application named "Web Application - Resource Capacity Plan for IBM Manage Service Project" for internal use.',
+      'Assisted in developing a report using JasperReports to convert the format and appearance of Crystal Reports.',
+      'Designed a flowchart to support the NSIPS system.',
+      'Find and fix Calendar issues in JSP file.',
+      'Studied Java Spring Boot framework courses to support and contribute to application development in the MT9XX MF Decom project.',
+    ],
+  },
+  {
+    title: 'Computer Science Information Technology Naresuan University',
+    url: 'https://csit.nu.ac.th/',
+    img: 'assets/work/csit.jpg',
+    imgDark: 'assets/work/csit.jpg',
+    duration: '',
+    role: 'Teacher assistant',
+    about: [
+      'Teacher assistant Teaching programming Object Oriented Programming 2/2022',
+      'Teacher assistant Teaching programming Object Oriented Programming 2/2021',
+      'Teacher assistant Teaching programming Python programming 1/2021',
+      'Teacher assistant Teaching programming Fundamental of programming 1/2021',
+      'Teacher assistant Teaching programming Object Oriented Programming 2/2020'
+    ],
+  },
+];
