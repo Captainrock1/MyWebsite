c2becda51d51453f833c80e64ed507af816fbbe0
Captainrock1 <66846381+Captainrock1@users.noreply.github.com>
2023-07-22 10:00:29 +0700

    Update projects.tsx
    Change topic name

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -18,7 +18,7 @@ const projects = () => {
             return <ProjectCard project={project} key={index} />;
           })}
         </div>
-        <h2 className="text-3xl md:text-5xl font-bold my-2">Team Projects</h2>
+        <h2 className="text-3xl md:text-5xl font-bold my-2">Group Projects</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
           {groupProjectsInfo.map((project, index) => {
             return <ProjectCard project={project} key={index} />;
